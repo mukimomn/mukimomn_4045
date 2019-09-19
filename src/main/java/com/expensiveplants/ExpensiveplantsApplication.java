a681c61464929f49df427e706631f36a0eb0cbb8
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-18 22:40:24 -0400

    Initial commit

---
+++ src/main/java/com/expensiveplants/ExpensiveplantsApplication.java
@@ -0,0 +1,13 @@
+package com.expensiveplants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensiveplantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensiveplantsApplication.class, args);
+	}
+
+}
